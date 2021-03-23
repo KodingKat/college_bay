88221cbd28406682792fcbcbbef60b0a998309cc
ray lo <lockhart.ray>
2021-03-23 15:57:33 -0500

    Login page

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: MyStatefulWidget(),
+      home: LoginPage(),
     );
   }
 }
@@ -29,6 +29,97 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('CollegeBay Login Page'),
+        ),
+        body: SingleChildScrollView(
+            child: Column(
+                children: <Widget>[
+                  Padding(
+                      padding: const EdgeInsets.only(top: 60.0),
+                      child: Center(
+                          child: Container(
+                            width: 200,
+                            height:150,
+                            child: Text(
+                              'CollegeBay Logo Space',
+                              style: TextStyle(
+                                  color: Colors.indigo, fontSize: 36),
+                            ),
+                          ),
+                      ),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'User Name',
+                          hintText: 'Enter school email address'
+                        ),
+                      ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        hintText: '8-16 characters'
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: (){
+
+                    },
+                    child: Text(
+                      'Forgot Password',
+                      style: TextStyle(color: Colors.indigo, fontSize: 15),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 250,
+                    decoration: BoxDecoration(
+                      color: Colors.indigo, borderRadius:
+                        BorderRadius.circular(20)),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context, MaterialPageRoute(
+                            builder: (_) => MyStatefulWidget()));
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 130,
+                  ),
+                  Text('New User? Create Account',
+                      style: TextStyle(color: Colors.indigo, fontSize: 15),
+                  ),
+                ],
+            ),
+        ),
+    );
+  }
+}
 class _MyHomePageState extends State<MyHomePage> {
   File _image;
   String _uploadedFileURL;
